3bf441c7aee3cf984be7b4374b74e26318a4e5d0
Flavia Brogle <flavia.brogle@unibas.ch>
2017-03-22 10:06:35 +0100

    ChatmParser now correctly validates the argument.

--- server-client/src/server/ChatmParser.java
+++ server-client/src/server/ChatmParser.java
@@ -18,51 +18,80 @@ public class ChatmParser {
 	
 	public ChatmParser(String argument){
         this.argument = argument;
-        if (validateArgument() == false) {
-        	System.err.println("Argument for command CHATM not properly formatted! Please use format chatm <sender_name> <recipient_name> <message>");
-	        // TODO: send this message back to the client.
-        	return;
-        }
+        
         handleChat();
     }
 	
-	private boolean validateArgument() {
-		// TODO
-		return false;
-	}
 	
 	private void handleChat() {
-    	separateArgument();
-    	
+		if (separateArgument() == false) {
+			System.err.println("Argument for command CHATM not properly formatted!\n" +
+					"Please use format: 'chatm <sender_name> <recipient_name> <message>'!\n" +
+					"Don't forget to surround the <message> with 'apostrophes'!");
+			// TODO: send this message back to the client.
+			return;
+		}
+		System.out.println("Message correctly received!");
+		
 	}
 	
 	/**
-	 * This method separates the given arguments into the three arguments needed for the Chat.
+	 * This method checks the argument and separates the given arguments into the three arguments needed for the Chat.
+	 * @return <code>false</code> when the argument isn't formatted correctly, <code>true</code> when everything's ok with the argument.
 	 */
-	private void separateArgument() {
+	private boolean separateArgument() {
     	char[] argumentChars = argument.toCharArray();
     	StringBuffer senderName = new StringBuffer();
     	StringBuffer recipientName = new StringBuffer();
     	StringBuffer message = new StringBuffer();
 		
-    	int argumentNr = 1;
+		int argumentNr = 1;
 		
+		// separates the arguments into the three arguments.
 		for (int i = 0; i < argumentChars.length; i++) {
 			if (argumentChars[i] == ' ' && argumentNr < 3) {
 				argumentNr++;
 				continue;
 			}
+			if (argumentNr == 1) {
+				senderName.append(argumentChars[i]);
+			} else if (argumentNr == 2) {
+				recipientName.append(argumentChars[i]);
+			}
 			if (argumentNr == 3) {
 				message.append(argumentChars[i]);
 				continue;
 			}
+			
 		}
+		
+		// Checks if argument is valid.
+		if (senderName.length() == 0)
+			return false;
+		
+		if (senderName.charAt(0) == '\'' && senderName.charAt(senderName.length() - 1) == '\'') {
+			senderName.deleteCharAt(senderName.length() - 1);
+			senderName.deleteCharAt(0);
+		}
+		
+		if (recipientName.length() == 0)
+			return false;
+		
+		if (recipientName.charAt(0) == '\'' && recipientName.charAt(recipientName.length() - 1) == '\'') {
+			recipientName.deleteCharAt(recipientName.length() - 1);
+			recipientName.deleteCharAt(0);
+		}
+		
+		if (message.length() == 0)
+			return false;
 		
 		if (message.charAt(0) == '\'' && message.charAt(message.length() - 1) == '\'') {
 			message.deleteCharAt(message.length() - 1);
 			message.deleteCharAt(0);
 		} else {
-			System.err.println("Message not properly formatted! validateArgument() went wrong!");
+			return false;
 		}
+		
+		return true;
 	}
 }
