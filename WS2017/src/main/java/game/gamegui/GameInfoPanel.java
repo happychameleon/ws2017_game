82e05cdcac9d1c631cb6fda257cd427f9efb3e72
Patrick <patrick.brehm@stud.unibas.ch>
2017-05-02 14:00:58 +0200

    first try

--- WS2017/src/main/java/game/gamegui/GameInfoPanel.java
+++ WS2017/src/main/java/game/gamegui/GameInfoPanel.java
@@ -1,9 +1,12 @@
 package game.gamegui;
 
 import game.engine.World;
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This is used below the main game panel to display info about the selected Tile and the game in general.
@@ -15,9 +18,35 @@ public class GameInfoPanel extends JPanel {
 	private final World world;
 	
 	public GameInfoPanel(World world, Window window) {
+		
 		this.world = world;
 		
 		setMinimumSize(new Dimension(15, 100));
 		
+		
+		JLabel wetness = new JLabel("Wetness: " + String.valueOf(6));
+		this.add(wetness);
+		JLabel weapon = new JLabel("Weapon: ");
+		this.add(weapon);
+		JLabel color = new JLabel("Your Color is: " + String.valueOf(world.getCurrentPlayer().getColor()));
+		this.add(color);
+		JLabel turn = new JLabel(String.valueOf(world.getCurrentPlayer().getName())+"'s turn");
+		this.add(turn);
+		
+		
+		
+		BufferedImage weaponPic;
+		try {
+			weaponPic = ImageIO.read(getClass().getResource("/images/tiles/attackRangeSprite.png"));
+			JLabel weaponPicLabel = new JLabel(new ImageIcon(weaponPic));
+			this.add(weaponPicLabel);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 	}
+	
 }
