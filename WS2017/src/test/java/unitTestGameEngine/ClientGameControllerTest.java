2988b546b0f86224b03b68c3acb37686f4443aab
maxh <m.hackinger@stud.unibas.ch>
2017-05-03 15:30:08 +0200

    New empty unit tests

---
+++ WS2017/src/test/java/unitTestGameEngine/ClientGameControllerTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by m on 02/05/17.
+ */
+public class ClientGameControllerTest {
+
+}
--- WS2017/src/test/java/unitTestGameEngine/TestClientGameController.java
+++
@@ -1,8 +0,0 @@
-package unitTestGameEngine;
-
-/**
- * Unit Test for clientGameController
- * Created by m on 26/04/17.
- */
-public class TestClientGameController {
-}
