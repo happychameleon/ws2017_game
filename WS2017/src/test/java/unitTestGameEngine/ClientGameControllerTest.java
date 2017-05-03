1008cc3a6140c502ec5284f941f49d883e0dd299
maxh <m.hackinger@stud.unibas.ch>
2017-05-03 15:32:50 +0200

    Changed and improved ClietGameControllerTest and GameMapTest

--- WS2017/src/test/java/unitTestGameEngine/ClientGameControllerTest.java
+++ WS2017/src/test/java/unitTestGameEngine/ClientGameControllerTest.java
@@ -1,8 +1,71 @@
+package unitTestGameEngine;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by m on 02/05/17.
  */
 public class ClientGameControllerTest {
+    @Test
+    public void getGameLobby() throws Exception {
+    }
+
+    @Test
+    public void removeGameLobby() throws Exception {
+    }
+
+    @Test
+    public void getWindow() throws Exception {
+    }
+
+    @Test
+    public void getStartScreen() throws Exception {
+    }
+
+    @Test
+    public void startGameForWatching() throws Exception {
+    }
+
+    @Test
+    public void askToJoinGame() throws Exception {
+    }
+
+    @Test
+    public void addUserToGame() throws Exception {
+    }
+
+    @Test
+    public void thisClientIsReady() throws Exception {
+    }
+
+    @Test
+    public void setUserAsWaiting() throws Exception {
+    }
+
+    @Test
+    public void sendStartGame() throws Exception {
+    }
+
+    @Test
+    public void startGame() throws Exception {
+    }
+
+    @Test
+    public void removeUser() throws Exception {
+    }
+
+    @Test
+    public void askToLeaveGame() throws Exception {
+    }
+
+    @Test
+    public void askToWatchGame() throws Exception {
+    }
+
+    @Test
+    public void endGame() throws Exception {
+    }
 
 }
--- WS2017/src/test/java/unitTestGameEngine/GameMapTest.java
+++ WS2017/src/test/java/unitTestGameEngine/GameMapTest.java
@@ -1,8 +1,79 @@
+package unitTestGameEngine;
+
+import game.GameMap;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by m on 02/05/17.
  */
 public class GameMapTest {
+    private String mapName;
+    private char[][] tiles;
+    private GameMap map;
+
+    @Before
+    public void create(){
+        mapName = "amap";
+        tiles = new char[][]{{'1', '1', 'G', '2'}, {'G', 'w', 'w', '2'}, {'3', 'W', 'W', 'G'}, {'3', 'G', '4', '4'}};
+        map = new GameMap(mapName, tiles);
+    }
+
+    @Test
+    public void getName() throws Exception {
+        String mapNameTest = map.getName();
+
+        boolean matches = mapNameTest.matches(mapName);
+        assertTrue(matches);
+    }
+
+    @Test
+    public void getTilesAsChars() throws Exception {
+        char[][] mapTest = map.getTilesAsChars();
+
+        boolean match = Arrays.equals(tiles, mapTest);
+        assertTrue(match);
+    }
+
+    @Test
+    public void getWidth() throws Exception {
+        int testWidth = 4;
+        int width = map.getWidth();
+
+        assertTrue(testWidth == width);
+
+    }
+
+    @Test
+    public void getHeight() throws Exception {
+        int testHeight = 4;
+        int height = map.getHeight();
+
+        assertTrue(testHeight == height);
+    }
+
+    @Test
+    public void getStartPositionCount() throws Exception {
+        int testPostionCount = 2;
+        int positionCount = map.getStartPositionCount();
+
+        assertTrue(testPostionCount == positionCount);
+    }
+
+    @Test
+    public void getAllMaps() throws Exception {
+    }
+
+    @Test
+    public void getMapForName() throws Exception {
+    }
+
+    @Test
+    public void readInAllMaps() throws Exception {
+    }
 
 }
