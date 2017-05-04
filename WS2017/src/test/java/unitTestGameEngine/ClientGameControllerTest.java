489e0a98cba96e90f1cb7886ce2ce71193ebbf77
maxh <m.hackinger@stud.unibas.ch>
2017-05-04 12:36:01 +0200

    small changes to unit test for ClientGameController

--- WS2017/src/test/java/unitTestGameEngine/ClientGameControllerTest.java
+++ WS2017/src/test/java/unitTestGameEngine/ClientGameControllerTest.java
@@ -1,8 +1,84 @@
 package unitTestGameEngine;
 
+import game.GameState;
+import org.junit.Before;
+import org.junit.Test;
+import serverclient.User;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
 /**
- * Unit Test for clientGameController
- * Created by m on 26/04/17.
+ * Created by m on 03/05/17.
  */
 public class ClientGameControllerTest {
+
+    @Before
+    public void setup(){
+        GameState gameState;
+        int startingPoints;
+        String gameName;
+        HashMap<User, String>
+    }
+    @Test
+    public void getGameLobby() throws Exception {
+    }
+
+    @Test
+    public void removeGameLobby() throws Exception {
+    }
+
+    @Test
+    public void getWindow() throws Exception {
+    }
+
+    @Test
+    public void getStartScreen() throws Exception {
+    }
+
+    @Test
+    public void startGameForWatching() throws Exception {
+    }
+
+    @Test
+    public void askToJoinGame() throws Exception {
+    }
+
+    @Test
+    public void addUserToGame() throws Exception {
+    }
+
+    @Test
+    public void thisClientIsReady() throws Exception {
+    }
+
+    @Test
+    public void setUserAsWaiting() throws Exception {
+    }
+
+    @Test
+    public void sendStartGame() throws Exception {
+    }
+
+    @Test
+    public void startGame() throws Exception {
+    }
+
+    @Test
+    public void removeUser() throws Exception {
+    }
+
+    @Test
+    public void askToLeaveGame() throws Exception {
+    }
+
+    @Test
+    public void askToWatchGame() throws Exception {
+    }
+
+    @Test
+    public void endGame() throws Exception {
+    }
+
 }
