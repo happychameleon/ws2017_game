6802689a24f888417837dada721ceab651dc52b4
maxh <m.hackinger@stud.unibas.ch>
2017-05-05 11:04:59 +0200

    small changes to unit tests

--- WS2017/src/test/java/unitTestGameEngine/ServerGameControllerTest.java
+++ WS2017/src/test/java/unitTestGameEngine/ServerGameControllerTest.java
@@ -1,7 +1,43 @@
 package unitTestGameEngine;
 
+import game.ClientGameController;
+import game.GameMap;
+import game.GameState;
+import game.ServerGameController;
+import org.junit.Before;
+import org.junit.Test;
+import server.ServerUser;
+import serverclient.User;
+
+import java.util.HashMap;
+
 /**
  * Created by m on 26/04/17.
  */
 public class ServerGameControllerTest {
+    private GameState gameState;
+    private int startingPoints;
+    private char[][] tiles;
+    private String testGameName;
+    private GameMap mapTest;
+    private ServerGameController serverGameController;
+
+    @Before
+    public void setup(){
+        GameMap.readInAllMaps();
+
+        gameState = GameState.STARTING;
+        startingPoints = 10;
+        testGameName = "TestGame";
+        HashMap<User, String> users = new HashMap<>();
+        mapTest = GameMap.getMapForName("SmallTestMap",false);
+
+        serverGameController = new ServerGameController(gameState,startingPoints,testGameName,users, mapTest);
+    }
+
+    @Test
+    public void testAddUserToGame(){
+        //ServerUser user = new ServerUser("serverusertest0");
+        //serverGameController.addUserToGame();
+    }
 }
